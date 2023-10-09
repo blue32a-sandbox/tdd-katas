62164cd4b2391fb11ab8764311cc608a23918614
blue32a <blue32a.jp@gmail.com>
2023-10-10 00:21:56 +0900

    Step4 時刻が06:00-12:00の場合は朝の挨拶をするーGreeter

--- src/Greeter/Greeter.js
+++ src/Greeter/Greeter.js
@@ -1,8 +1,15 @@
 import capitalize from 'lodash/capitalize';
 
 export default class Greeter {
+  constructor(date) {
+    this._date = date;
+  }
   greet(name) {
     const filteredName = capitalize(name.trim());
+    if (this.morning) return `Good morning ${filteredName}`;
     return `Hello ${filteredName}`;
+  }
+  get morning() {
+    return this._date.getHours() >= 6 && this._date.getHours() < 12;
   }
 }
--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -2,22 +2,40 @@ import Greeter from './Greeter';
 
 describe('挨拶をする Greeter クラス', () => {
   describe('greet メソッドは引数の名前に対して、文字列で挨拶を返す', () => {
-    test('文字列 "Hello <名前>" を返す', () => {
-      const sut = new Greeter();
+    test.each([
+      [6],
+      [9],
+      [11],
+    ])('時刻が06:00-12:00の場合、文字列 "Good morning <名前>" を返す (%i:00)', (hours) => {
+      const sut = factoryGreeter(factoryDate(hours));
+
+      const result = sut.greet('John');
+
+      expect(result).toBe('Good morning John');
+    });
+    test.each([
+      [0],
+      [3],
+      [5],
+      [12],
+      [17],
+      [23],
+    ])('それ以外の時刻の場合、文字列 "Hello <名前>" を返す (%i:00)', (hours) => {
+      const sut = factoryGreeter(factoryDate(hours));
 
       const result = sut.greet('John');
 
       expect(result).toBe('Hello John');
     });
     test('名前をトリムする', () => {
-      const sut = new Greeter();
+      const sut = factoryGreeter();
 
       const result = sut.greet('  John  ');
 
       expect(result).toBe('Hello John');
     });
     test('名前の最初の文字を大文字にする', () => {
-      const sut = new Greeter();
+      const sut = factoryGreeter();
 
       const result = sut.greet('john');
 
@@ -25,3 +43,14 @@ describe('挨拶をする Greeter クラス', () => {
     });
   })
 });
+
+function factoryGreeter(date) {
+  date = typeof date === "undefined" ? new Date() : date;
+  return new Greeter(date);
+}
+
+function factoryDate(hours) {
+  const result = new Date();
+  result.setHours(hours);
+  return result
+}
