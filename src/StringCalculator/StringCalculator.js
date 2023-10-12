8e35b67e4666e1000258ffe364be96c729bb7e3e
blue32a <blue32a.jp@gmail.com>
2023-10-12 12:57:47 +0900

    refine ーString Calculator

--- src/StringCalculator/StringCalculator.js
+++ src/StringCalculator/StringCalculator.js
@@ -3,26 +3,31 @@ export default class StringCalculator {
     if (str === '') return 0;
 
     const nums = this._parse(str);
+    this._assertNoNegative(nums);
 
-    const negatives = nums.filter((v) => v < 0);
-    if (negatives.length > 0) {
-      throw new Error(`negatives not allowed: ${negatives.join(',')}`);
-    }
-
-    return nums.reduce((previous, current) => previous + current, 0);
+    return this._sum(nums);
   }
   _parse(input) {
     let delimiter = /[,\n]/;
-    let data = input;
+    let dataStr = input;
     const lines = input.split('\n');
 
     if (lines[0].startsWith('//')) {
       delimiter = lines[0].slice(2);
-      data = lines[1];
+      dataStr = lines[1];
     }
 
-    return data.split(delimiter)
-              .map((v) => parseInt(v))
-              .filter((v) => v < 1000);
+    return dataStr.split(delimiter)
+              .map((s) => parseInt(s))
+              .filter((i) => i < 1000);
+  }
+  _assertNoNegative(nums) {
+    const negatives = nums.filter((i) => i < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negatives not allowed: ${negatives}`);
+    }
+  }
+  _sum(nums) {
+    return nums.reduce((previous, current) => previous + current, 0);
   }
 }
--- src/StringCalculator/StringCalculator.test.js
+++ src/StringCalculator/StringCalculator.test.js
@@ -2,66 +2,60 @@ import StringCalculator from './StringCalculator';
 
 describe('文字列についての計算を行う StringCalculator クラス', () => {
   describe('add メソッドは与えられた文字列内の数値の和を返す', () => {
+    let sut;
+    beforeEach(() => {
+      sut = new StringCalculator();
+    });
+
     test('空の文字列は 0 を返す', () => {
-      const sut = new StringCalculator();
-
-      const result = sut.add('');
-
-      expect(result).toBe(0);
+      expect(sut.add('')).toBe(0);
     });
-    test('単一の数字からなる文字列はそのまま数値に変換して返す', () => {
-      const sut = new StringCalculator();
 
-      const result = sut.add('2');
-
-      expect(result).toBe(2);
+    test.each([
+      ['1', 1],
+      ['2', 2],
+    ])('単一の数字からなる文字列はそのまま数値に変換して返す ("%s" => %i)', (input, expected) => {
+      expect(sut.add(input)).toBe(expected);
     });
-    test('カンマで区切られた数字は、その合計を数値で返す', () => {
-      const sut = new StringCalculator();
 
-      const result = sut.add('1,2');
-
-      expect(result).toBe(3);
+    test.each([
+      ['1,2', 3],
+      ['3,4', 7],
+    ])('カンマで区切られた数字は、その合計を数値で返す ("%s" => %i)', (input, expected) => {
+      expect(sut.add(input)).toBe(expected);
     });
-    test('改行で区切られた数字は、その合計を数値で返す', () => {
-      const sut = new StringCalculator();
 
-      const result = sut.add('1\n2');
-
-      expect(result).toBe(3);
+    test.each([
+      ['1\n2', 3],
+      ['3\n4', 7],
+    ])('改行で区切られた数字は、その合計を数値で返す ("%s" => %i)', (input, expected) => {
+      expect(sut.add(input)).toBe(expected);
     });
+
     test('区切文字がカンマと改行の両方含まれていても、その合計を数値で返す', () => {
-      const sut = new StringCalculator();
-
-      const result = sut.add('1\n2,3\n4');
-
-      expect(result).toBe(10);
+      expect(sut.add('1\n2,3\n4')).toBe(10);
     });
-    test('負の数が含まれる場合は例外をスローする', () => {
-      const sut = new StringCalculator();
 
+    test('負の数が含まれる場合は例外をスローする', () => {
       expect(() => sut.add('-1,2,-3')).toThrow('negatives not allowed: -1,-3');
     });
+
     test('1000以上の数は無視される', () => {
-      const sut = new StringCalculator();
-
-      const result = sut.add('1,1000,2');
-
-      expect(result).toBe(3);
+      expect(sut.add('1,1000,2')).toBe(3);
     });
-    test('//で始まる１行目がある場合、//の後に続く単一の文字を区切りとして、２行目の数字の合計を返す', () => {
-      const sut = new StringCalculator();
 
-      const result = sut.add('//#\n1#2');
-
-      expect(result).toBe(3);
+    test.each([
+      ['//#\n1#2', 3],
+      ['//-\n3-4', 7],
+    ])('//で始まる１行目がある場合、//の後に続く単一の文字を区切りとして、２行目の数字の合計を返す ("%s" => %i)', (input, expected) => {
+      expect(sut.add(input)).toBe(expected);
     });
-    test('//で始まる１行目がある場合、//の後に続く文字列を区切りとして、２行目の数字の合計を返す', () => {
-      const sut = new StringCalculator();
 
-      const result = sut.add('//###\n1###2');
-
-      expect(result).toBe(3);
+    test.each([
+      ['//###\n1###2', 3],
+      ['//-=a1\n3-=a14', 7],
+    ])('//で始まる１行目がある場合、//の後に続く文字列を区切りとして、２行目の数字の合計を返す ("%s" => %i)', (input, expected) => {
+      expect(sut.add(input)).toBe(expected);
     });
   });
 });
