d2806d42895b1c3160536c8c959129b70103fd04
blue32a <blue32a.jp@gmail.com>
2023-10-13 18:33:09 +0900

    Step3. 整数3のとき、`[3]`を返す ーPrime Factors

--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -1,5 +1,5 @@
 export function generate(int) {
   const primes = [];
-  if (int > 1) primes.push(2);
+  if (int > 1) primes.push(int);
   return primes;
 }
--- src/PrimeFactors/PrimeFactors.test.js
+++ src/PrimeFactors/PrimeFactors.test.js
@@ -8,5 +8,8 @@ describe('素因数分解について扱う PrimeFactors モジュール', () =>
     test('整数 2 のとき、 [2] を返す', () => {
       expect(generate(2)).toEqual([2]);
     });
+    test('整数 3 のとき、 [3] を返す', () => {
+      expect(generate(3)).toEqual([3]);
+    });
   });
 });
