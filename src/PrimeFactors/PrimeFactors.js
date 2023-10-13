3b7dd0a2584fb1d4f23545e4f243ad1aa0b44b80
blue32a <blue32a.jp@gmail.com>
2023-10-13 19:01:11 +0900

    refine ーPrime Factors

--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -1,15 +1,9 @@
 export function generate(int) {
   const primes = [];
-  let candidate = 2;
-  while (int > 1) {
-    while (int % candidate === 0) {
+  for (let candidate = 2; int > 1; candidate++) {
+    for (; int % candidate === 0; int /= candidate) {
       primes.push(candidate);
-      int /= candidate;
     }
-    candidate++;
-  }
-  if (int > 1) {
-    primes.push(int);
   }
   return primes;
 }
