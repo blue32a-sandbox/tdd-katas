3848cc233a2eb070274efd2e57f23218d69d233b
blue32a <blue32a.jp@gmail.com>
2024-02-09 12:39:12 +0900

    リファクタリング ー Gilded Rose

--- src/GildedRose/GildedRose.js
+++ src/GildedRose/GildedRose.js
@@ -7,56 +7,73 @@ export class Item {
 }
 
 export class Shop {
-  constructor(items=[]){
+  constructor(items = []){
     this.items = items;
   }
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
+    this.items.forEach((item) => this._apply(item));
     return this.items;
+  }
+  _apply(item) {
+    if (this._isSulfuras(item)) {
+      // do nothing
+    } else if (this._isAgedBrie(item)) {
+      item.sellIn--;
+      item.quality = this._agedBrieNewQuality(item).value;
+    } else if (this._isBackstagePasses(item)) {
+      const oldSellIn = item.sellIn;
+      item.sellIn--;
+      item.quality = this._backstagePassesNewQuality(item, oldSellIn).value;
+    } else {
+      item.sellIn--;
+      item.quality = this._otherItemNewQuality(item).value;
+    }
+  }
+  _isSellByDatePassed(item) {
+    return item.sellIn < 0;
+  }
+  _isAgedBrie(item) {
+    return item.name === 'Aged Brie';
+  }
+  _isSulfuras(item) {
+    return item.name === 'Sulfuras, Hand of Ragnaros';
+  }
+  _isBackstagePasses(item) {
+    return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  }
+  _agedBrieNewQuality(item) {
+    const increases = this._isSellByDatePassed(item) ? 2 : 1;
+    return new Quality(item.quality).increase(increases);
+  }
+  _backstagePassesNewQuality(item, oldSellIn) {
+    if (this._isSellByDatePassed(item)) {
+      return new Quality(0);
+    }
+    let increases = 1;
+    if (oldSellIn <= 5) {
+      increases = 3;
+    } else if (oldSellIn <= 10) {
+      increases = 2;
+    }
+    return new Quality(item.quality).increase(increases);
+  }
+  _otherItemNewQuality(item) {
+    const drops = this._isSellByDatePassed(item) ? 2 : 1;
+    return new Quality(item.quality).drop(drops);
+  }
+}
+
+class Quality {
+  constructor(value) {
+    if (value < this.min) throw new Error('too small');
+    this.value = value;
+  }
+  get min() { return 0; }
+  get max() { return 50; }
+  increase(num) {
+    return new Quality(Math.min(this.max, this.value + num));
+  }
+  drop(num) {
+    return new Quality(Math.max(this.min, this.value - num));
   }
 }
--- src/GildedRose/GildedRose.test.js
+++ src/GildedRose/GildedRose.test.js
@@ -1,9 +1,95 @@
 import { Item, Shop } from './GildedRose';
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+  describe('Sulfuras', () => {
+    test('売られたり品質が下がったりすることはない', () => {
+      const gildedRose = factoryShop([factorySulfuras({sellIn: 0, quality: 80})]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(80);
+    });
+  });
+  describe('Aged Brie', () => {
+    test('古くなるほど品質が高くなる', () => {
+      const gildedRose = factoryShop([factoryAgedBrie({sellIn: 1, quality: 0})]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(1);
+    });
+    test('期限を過ぎると品質が２倍高くなる', () => {
+      const gildedRose = factoryShop([factoryAgedBrie({sellIn: 0, quality: 0})]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(2);
+    });
+  });
+  describe('Backstage passes', () => {
+    test('SellInが近づくほど価値が上がり、SellInを過ぎると価値がなくなる', () => {
+      const gildedRose = factoryShop([
+        factoryBackstagePasses({sellIn: 11, quality: 10}),
+        factoryBackstagePasses({sellIn: 10, quality: 10}),
+        factoryBackstagePasses({sellIn: 5, quality: 10}),
+        factoryBackstagePasses({sellIn: 0, quality: 10}),
+      ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(11);
+      expect(items[1].quality).toBe(12);
+      expect(items[2].quality).toBe(13);
+      expect(items[3].quality).toBe(0);
+    });
+  });
+  describe('その他の商品', () => {
+    test('古くなるほど品質が下がる', () => {
+      const gildedRose = factoryShop([factoryItem({sellIn: 2, quality: 2})]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(1);
+      expect(items[0].quality).toBe(1);
+    });
+    test('販売期限を過ぎると品質は2倍下がる', () => {
+      const gildedRose = factoryShop([factoryItem({sellIn: 0, quality: 4})]);
+      console.log(factoryItem({sellIn: 0, quality: 4}));
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(2);
+    });
+    test('品質はマイナスになることはない', () => {
+      const gildedRose = factoryShop([factoryItem({quality: 0})]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+  });
+  describe('共通', () => {
+    test('品質は50を超えない', () => {
+      const gildedRose = factoryShop([
+        factoryAgedBrie({quality: 50}),
+        factoryBackstagePasses({sellIn: 11, quality: 50})
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+      expect(items[1].quality).toBe(50);
+    });
   });
 });
+
+function factoryShop(items) {
+  return new Shop(items);
+}
+
+function factoryItem(params) {
+  params.name ??= 'foo';
+  params.sellIn ??= 1;
+  params.quality ??= 10;
+  return new Item(params.name, params.sellIn, params.quality);
+}
+
+function factoryAgedBrie(params) {
+  return factoryItem({name: 'Aged Brie', ...params});
+}
+
+function factorySulfuras(params) {
+  return factoryItem({name: 'Sulfuras, Hand of Ragnaros', ...params});
+}
+
+function factoryBackstagePasses(params) {
+  return factoryItem({name: 'Backstage passes to a TAFKAL80ETC concert', ...params});
+}
