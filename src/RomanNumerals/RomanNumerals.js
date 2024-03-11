379ae7d745a06ce1ece098f9c765b47ca2052e02
blue32a <blue32a.jp@gmail.com>
2024-03-11 21:32:01 +0900

    リファクタリング ー Roman Numerals

--- src/RomanNumerals/RomanNumerals.js
+++ src/RomanNumerals/RomanNumerals.js
@@ -1,22 +1,14 @@
-export function convert(int) {
-  let result = '';
-
-  const thousands = Math.floor(int / 1000);
-  if (thousands === 1) result += 'M';
-
-  const hundreds = Math.floor((int % 1000) / 100);
-  result += hundredsConverter(hundreds);
-
-  const tens = Math.floor((int % 100) / 10);
-  result += tensConverter(tens);
-
-  const once = int % 10;
-  result += onceConverter(once);
-
-  return result;
+function placeValueExtractor(place) {
+  return function (int) {
+    return Math.floor(int / place) % 10;
+  }
 }
+const extractOnce      = placeValueExtractor(1);
+const extractTens      = placeValueExtractor(10);
+const extractHundreds  = placeValueExtractor(100);
+const extractThousands = placeValueExtractor(1000);
 
-function converter(lower, middle, upper) {
+function baseConverter(lower, middle, upper) {
   return function (int) {
     if (int <= 3)  return lower.repeat(int);
     if (int === 4) return lower + middle;
@@ -25,7 +17,19 @@ function converter(lower, middle, upper) {
     return '';
   }
 }
+const convertOnce      = baseConverter('I', 'V', 'X');
+const convertTens      = baseConverter('X', 'L', 'C');
+const convertHundreds  = baseConverter('C', 'D', 'M');
+const convertThousands = (thousands) => (thousands === 1) ? 'M' : '';
 
-const onceConverter     = converter('I', 'V', 'X');
-const tensConverter     = converter('X', 'L', 'C');
-const hundredsConverter = converter('C', 'D', 'M');
+export function convert(int) {
+  const rules = [
+    { extract: extractThousands, convert: convertThousands },
+    { extract: extractHundreds, convert: convertHundreds },
+    { extract: extractTens, convert: convertTens },
+    { extract: extractOnce, convert: convertOnce }
+  ];
+  return rules.reduce((result, { extract, convert }) => {
+    return result + convert(extract(int));
+  }, '');
+}
